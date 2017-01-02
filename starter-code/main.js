b44150517f04d303c5edafe223e85f3378e5e36e
Cathy OBrien <cathy.obrien09@gmail.com>
2017-01-02 18:53:05 -0500

    new game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,6 +3,7 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*
 if (cardOne === cardTwo) {
   alert ("You found a match!")
 } else {
@@ -18,3 +19,19 @@ if (cardTwo === cardFour) {
 } else {
   alert ("Sorry, try again.")
 }
+*/
+
+var numberOfCards = 4 //so can be updated easily
+
+var createCards = function() {
+  var board = document.getElementById('game-board');
+
+  for (var i = 0; i < numberOfCards; i++) {
+    var newCard = document.createElement('div')
+    newCard.className = 'card'
+
+    board.appendChild(newCard)
+  }
+}
+
+createCards()
