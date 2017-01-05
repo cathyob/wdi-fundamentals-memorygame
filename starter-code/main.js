53b557c6f52dd1eb4b439d92cd3347076af5cb99
Cathy OBrien <cathy.obrien09@gmail.com>
2017-01-04 23:13:23 -0500

    delete comments

--- starter-code/main.js
+++ starter-code/main.js
@@ -39,39 +39,3 @@ var isTwoCards = function() {
 }
 
 createBoard()
-
-/*var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-
-if (cardOne === cardTwo) {
-  alert ("You found a match!")
-} else {
-  alert ("Sorry, try again.")
-}
-if (cardThree === cardFour) {
-  alert ("You found a match!")
-} else {
-  alert ("Sorry, try again.")
-}
-if (cardTwo === cardFour) {
-  alert ("You found a match!")
-} else {
-  alert ("Sorry, try again.")
-}
-var numberOfCards = 4 //so can be updated easily
-
-var createCards = function() {
-  var board = document.getElementById('game-board');
-
-  for (var i = 0; i < numberOfCards; i++) {
-    var newCard = document.createElement('div')
-    newCard.className = 'card'
-
-    board.appendChild(newCard)
-  }
-}
-
-createCards()
-*/
